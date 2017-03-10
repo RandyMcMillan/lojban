05d31ab2ea336d0a47383bcd60c05fe440194e0f
Randy McMillan <randy.lee.mcmillan@gmail.com>
2017-03-10 17:35:47 -0500

    git add * ; uncrustify; git commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ ! -d /Volumes/Auxiliary\ Tools ]; then
+
+open ./Auxiliary_Tools_for_Xcode_7.dmg
+
+fi
+
+if [ ! -d /DevTools/ ]; then
+
+mkdir /DevTools
+
+fi
+
+if [ ! -d /DevTools/Utilites ]; then
+
+mkdir /DevTools/Utilities
+
+fi
+
+if [ ! -d /DevTools/Utilities/Dictionary\ Development\ Kit/ ]; then
+
+cp /Volumes/Auxiliary Tools/Dictionary\ Development\ Kit /DevTools/Utilities/ictionary\ Development\ Kit/
+
+fi
